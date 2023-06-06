6aa97cee781391f1dd4d6fbf1bb0889d18e26d20
Sky <nebulass@naver.com>
2023-06-06 23:56:11 +0900

    recoil atom value 변경하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { useSetRecoilState } from "recoil";
 import { Container } from "../components/container";
 import { Header } from "../components/header";
+import { isDarkAtom } from "../state/atoms";
 
 const Home = () => {
+  // 아톰의 value를 변경하기 위해 useSetRecoilState 사용
+  const setterTheme = useSetRecoilState(isDarkAtom);
+
   return (
     <>
       <Header>
         Header
-        <button>change theme</button>
+        <button onClick={() => setterTheme((prev) => !prev)}>
+          change theme
+        </button>
       </Header>
       <Container>
         <h1>gh</h1>
