2d6013b9281a18e5af218f37a61ea6cacce209a4
Sky <nebulass@naver.com>
2023-06-06 23:48:14 +0900

    리코일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import GlobalStyle from "./style/globalStyles";
 import { useState } from "react";
 
 function App() {
-  const [theme, setTheme] = useState(false);
-  const handleToggleTheme = () => setTheme((prev) => !prev);
   return (
     <>
-      <ThemeProvider theme={theme ? darkTheme : lightTheme}>
+      <ThemeProvider theme={darkTheme}>
         <GlobalStyle />
         <Router />
 
