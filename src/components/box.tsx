88f20125a14f4ca969f000097a3d78522c0cce67
Sky <nebulass@naver.com>
2023-06-11 18:50:10 +0900

    자식에게 모션 주기

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -4,7 +4,17 @@ import { styled } from "styled-components";
 export const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 30px;
+`;
+
+export const Circle = styled(motion.div)`
   background-color: #fff;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  place-self: center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1);
 `;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,39 @@ import { isDarkAtom, todoState } from "../state/atoms";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Wrapper } from "../components/wrapper";
-import { Box } from "../components/box";
+import { Box, Circle } from "../components/box";
 import { motion } from "framer-motion";
 
 const TodoList = () => {
-  const animation = {
-    start: { scale: 0 },
+  const boxAnimation = {
+    start: {
+      opacity: 0,
+      scale: 0.5,
+    },
     end: {
+      opacity: 1,
       scale: 1,
-      rotateZ: 360,
       transition: {
-        type: "spring",
-        damping: 10,
+        type: "string",
+        duration: 0.5,
+        bounce: 0.5,
+        delayChildren: 0.5,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const circleAnimation = {
+    start: {
+      opacity: 0,
+      y: 10,
+    },
+    end: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "string",
+        bounce: 0.5,
       },
     },
   };
@@ -22,8 +43,12 @@ const TodoList = () => {
   return (
     <>
       <Wrapper>
-        <Box variants={animation} initial="start" animate="end" />
-        {/* <motion.div></motion.div> */}
+        <Box variants={boxAnimation} initial="start" animate="end">
+          <Circle variants={circleAnimation} />
+          <Circle variants={circleAnimation} />
+          <Circle variants={circleAnimation} />
+          <Circle variants={circleAnimation} />
+        </Box>
       </Wrapper>
     </>
   );
